2b1e62736945984f8a6e65d95eca9411977e2c01
Adwaith <adwaithjayan124@gmail.com>
2025-04-12 12:01:49 +0530

    play audio

--- src/app/(protected)/player.tsx
+++ src/app/(protected)/player.tsx
@@ -1,10 +1,60 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Pressable, Image } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+import { router } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function Player() {
+import dummyBooks from '@/dummyBooks';
+import PlaybackBar from '@/components/playbackBar';
+
+import { useAudioPlayer, useAudioPlayerStatus } from 'expo-audio';
+
+export default function PlayerScreen() {
+  const book = dummyBooks[0];
+
+  const player = useAudioPlayer({ uri: book.audio_url });
+  const playerStatus = useAudioPlayerStatus(player);
+
+  console.log(JSON.stringify(playerStatus, null, 2));
+
   return (
-    <View>
-      <Text>player</Text>
-    </View>
-  )
+    <SafeAreaView className='flex-1  p-4 py-10 gap-4'>
+      <Pressable
+        onPress={() => router.back()}
+        className='absolute top-16 left-4 bg-gray-800 rounded-full p-2'
+      >
+        <Entypo name='chevron-down' size={24} color='white' />
+      </Pressable>
+
+      <Image
+        source={{ uri: book.thumbnail_url }}
+        className='w-[95%] aspect-square rounded-[30px] self-center'
+      />
+
+      <View className='gap-8 flex-1 justify-end'>
+        <Text className='text-white text-2xl font-bold text-center'>
+          {book.title}
+        </Text>
+        <PlaybackBar
+          currentTime={playerStatus.currentTime}
+          duration={playerStatus.duration}
+        />
+
+        <View className='flex-row items-center justify-between'>
+          <Ionicons name='play-skip-back' size={24} color='white' />
+          <Ionicons name='play-back' size={24} color='white' />
+          <Ionicons
+            onPress={() =>
+              playerStatus.playing ? player.pause() : player.play()
+            }
+            name={playerStatus.playing ? 'pause' : 'play'}
+            size={50}
+            color='white'
+          />
+          <Ionicons name='play-forward' size={24} color='white' />
+          <Ionicons name='play-skip-forward' size={24} color='white' />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
--- src/components/bookListItem.tsx
+++ src/components/bookListItem.tsx
@@ -1,5 +1,7 @@
-import {  Image, Text, View } from 'react-native';
+import {  Image, Text, View ,Pressable} from 'react-native';
 import {AntDesign} from '@expo/vector-icons';
+import {Link} from 'expo-router'
+
 
 type Book = {
     book:{
@@ -14,13 +16,15 @@ type Book = {
 
 export default function BookListItem({book}: Book) {
   return (
-    <View className='flex-row gap-4 items-center'>
+    <Link href='/player' asChild>
+    <Pressable className='flex-row gap-4 items-center'>
       <Image source={{ uri: book.thumbnail_url }} className="size-24 rounded-md aspect-squre"/>
       <View className="gap-1 flex-1">
       <Text className="text-2xl text-gray-100 font-bold">{book.title}</Text>
       <Text className="text-gray-400">{book.author}</Text>
       </View>
       <AntDesign name='playcircleo' size={24} color='gainsboro'/>
-    </View>
+    </Pressable>
+    </Link>
   )
 }

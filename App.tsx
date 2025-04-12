8fb51552c0f74c1fa6fbe150062c34f96a85d625
Adwaith <adwaithjayan124@gmail.com>
2025-04-12 10:46:54 +0530

    books flatlist

--- App.tsx
+++ App.tsx
@@ -1,15 +1,13 @@
 import "./global.css";
 import { StatusBar } from 'expo-status-bar';
-import {   View } from 'react-native';
+import { FlatList,  View } from 'react-native';
 import dummyBooks from "./src/dummyBooks";
 import BookListItem from "./src/components/bookListItem";
 
 export default function App() {
   return (
-    <View className="flex flex-1 bg-slate-800 justify-center p-4" >
-      
-      <BookListItem book={dummyBooks[0]} />
-      <BookListItem book={dummyBooks[1]} />
+    <View className="flex flex-1 bg-slate-800 justify-center p-4 pt-20" >
+      <FlatList data={dummyBooks} renderItem={({item})=><BookListItem book={item} key={item.id}/>}  contentContainerClassName="gap-4"/>
       <StatusBar style="light" />
     </View>
   );
--- src/components/bookListItem.tsx
+++ src/components/bookListItem.tsx
@@ -15,7 +15,7 @@ type Book = {
 export default function BookListItem({book}: Book) {
   return (
     <View className='flex-row gap-4 items-center'>
-      <Image source={{ uri: book.thumbnail_url }} className="size-24 rounded-md"/>
+      <Image source={{ uri: book.thumbnail_url }} className="size-24 rounded-md aspect-squre"/>
       <View className="gap-1 flex-1">
       <Text className="text-2xl text-gray-100 font-bold">{book.title}</Text>
       <Text className="text-gray-400">{book.author}</Text>

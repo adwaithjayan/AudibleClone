705868342301a25f57dede4aba72a6760e7d26e3
Adwaith <adwaithjayan124@gmail.com>
2025-04-12 10:39:38 +0530

    booklist items

--- App.tsx
+++ App.tsx
@@ -1,21 +1,17 @@
 import "./global.css";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {   View } from 'react-native';
+import dummyBooks from "./src/dummyBooks";
+import BookListItem from "./src/components/bookListItem";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text className="text-2xl text-red-500">Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex flex-1 bg-slate-800 justify-center p-4" >
+      
+      <BookListItem book={dummyBooks[0]} />
+      <BookListItem book={dummyBooks[1]} />
+      <StatusBar style="light" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/bookListItem.tsx
@@ -0,0 +1,26 @@
+import {  Image, Text, View } from 'react-native';
+import {AntDesign} from '@expo/vector-icons';
+
+type Book = {
+    book:{
+        id: string,
+        title: string,
+        author: string,
+        audio_url:string,
+        thumbnail_url?:string
+    }
+}
+
+
+export default function BookListItem({book}: Book) {
+  return (
+    <View className='flex-row gap-4 items-center'>
+      <Image source={{ uri: book.thumbnail_url }} className="size-24 rounded-md"/>
+      <View className="gap-1 flex-1">
+      <Text className="text-2xl text-gray-100 font-bold">{book.title}</Text>
+      <Text className="text-gray-400">{book.author}</Text>
+      </View>
+      <AntDesign name='playcircleo' size={24} color='gainsboro'/>
+    </View>
+  )
+}
